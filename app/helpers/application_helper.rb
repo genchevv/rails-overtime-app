3755b98538e325a6b07f4a180424e3f328ce42e3
Radoslav Genchev <r.genchew@gmail.com>
2017-07-22 09:45:15 +0000

    Integrated application helper for status labels and integrated enums

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,25 @@ module ApplicationHelper
     def active?(path)
        "active" if current_page?(path) 
     end
+    
+    def status_label status
+        status_span_generator status
+    end
+    
+    private
+    
+    def status_span_generator status
+        case status
+        when 'submitted'
+            return content_tag(:span, status.titleize, class: 'label label-primary')
+        when 'approved'
+            return content_tag(:span, status.titleize, class: 'label label-success')          
+        when 'rejected'
+            return content_tag(:span, status.titleize, class: 'label label-danger')
+        when 'pending'
+            return content_tag(:span, status.titleize, class: 'label label-primary')
+        when 'confirmed'
+            return content_tag(:span, status.titleize, class: 'label label-success')    
+        end
+    end
 end
