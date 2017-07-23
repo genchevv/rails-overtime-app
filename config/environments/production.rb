ea2992c7fa09105de7648d3f52c1735d7fb3bf99
Radoslav Genchev <r.genchew@gmail.com>
2017-07-23 17:31:06 +0000

    Added devise secret key to the app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
-  Rails.application.config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
 end
